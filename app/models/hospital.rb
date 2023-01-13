d9d8d29c5f0a7a954c5bb812b72e48a4865ed250
Samuel Cox <samc1253@gmail.com>
2023-01-13 10:38:59 -0700

    Add method to return doctors for a hospital ordered by patient count

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,7 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctors_by_patient_count
+    self.doctors.joins(:doctor_patients).select('doctors.*, count(doctor_patients) as patient_count').group(:id).order('patient_count DESC')
+  end
 end
