7dffd9a0f9956cc2494d3b14a358763aa358a2ba
Samuel Cox <samc1253@gmail.com>
2023-01-13 09:36:12 -0700

    Create tests for doctor show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'Doctor Show' do
     @hospital_1 = Hospital.create!({name: 'Denver Health'})
     @hospital_2 = Hospital.create!({name: 'Porter Advnentist'})
 
-    @doctor_1 = @hospital_1.doctors.create!({name: 'Brian Cox', speciality: 'Cardiac', university: 'Johns Hopkins'})
-    @doctor_2 = @hospital_1.doctors.create!({name: 'Erin Cox', speciality: 'Spinal', university: 'School of Mines'})
-    @doctor_3 = @hospital_2.doctors.create!({name: 'Samuel Cox', speciality: 'Neurological', university: 'Fordham'})
+    @doctor_1 = @hospital_1.doctors.create!({name: 'Brian Cox', specialty: 'Cardiac', university: 'Johns Hopkins'})
+    @doctor_2 = @hospital_1.doctors.create!({name: 'Erin Cox', specialty: 'Spinal', university: 'School of Mines'})
+    @doctor_3 = @hospital_2.doctors.create!({name: 'Samuel Cox', specialty: 'Neurological', university: 'Fordham'})
 
     @patient_1 = Patient.create!({name: Faker::FunnyName.name, age: 26})
     @patient_2 = Patient.create!({name: Faker::FunnyName.name, age: 48})
@@ -22,5 +22,49 @@ RSpec.describe 'Doctor Show' do
     DoctorPatient.create!({doctor_id: @doctor_2.id, patient_id: @patient_4.id})
     DoctorPatient.create!({doctor_id: @doctor_3.id, patient_id: @patient_5.id})
     DoctorPatient.create!({doctor_id: @doctor_3.id, patient_id: @patient_1.id})
+  end
+
+  it ' shows all doctor information' do
+    visit doctor_path(@doctor_1)
+
+    expect(page).to have_content(@doctor_1.name)
+    expect(page).to have_content(@doctor_1.speciality)
+    expect(page).to have_content(@doctor_1.university)
+    expect(page).to_not have_content(@doctor_2.name)
+
+    visit doctor_path(@doctor_2)
+
+    expect(page).to have_content(@doctor_2.name)
+    expect(page).to have_content(@doctor_2.speciality)
+    expect(page).to have_content(@doctor_2.university)
+    expect(page).to_not have_content(@doctor_1.name)
+  end
+
+  it 'shows the hospital where the doctor works' do
+    visit doctor_path(@doctor_1)
+
+    expect(page).to have_content(@hospital_1.name)
+    expect(page).to_not have_content(@hospital_2.name)
+
+    visit doctor_path(@doctor_3)
+
+    expect(page).to have_content(@hospital_2.name)
+    expect(page).to_not have_content(@hospital_1.name)
+  end
+
+  it 'shows the names of all patients for the doctor' do
+    visit doctor_path(@doctor_1)
+
+    expect(page).to have_content(@patient_1.name)
+    expect(page).to have_content(@patient_2.name)
+    expect(page).to have_content(@patient_3.name)
+    expect(page).to_not have_content(@patient_4.name)
+
+    visit doctor_path(@doctor_2)
+
+    expect(page).to_not have_content(@patient_1.name)
+    expect(page).to_not have_content(@patient_2.name)
+    expect(page).to have_content(@patient_3.name)
+    expect(page).to have_content(@patient_4.name)
   end
 end
